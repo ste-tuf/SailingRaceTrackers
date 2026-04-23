6a2ddaad6cb83991347f5cd68fce3f52ec1d5ccc
Stéphane Tuffier <stephane.tuffier@sund.ku.dk>
2026-04-23 19:52:56 +0200

    Remove R scripts and Quarto data

--- R/load_config.R
+++
@@ -1,8 +0,0 @@
-#' Load and parse XML configuration file
-#' @param path_file Path to XML config file
-#' @return Parsed XML document
-load_config <- function(path_file) {
-    xml_content <- readLines(path_file, encoding = "UTF-8", warn = FALSE)
-    xml_content <- paste(xml_content, collapse = "\n")
-    read_xml(xml_content)
-}
