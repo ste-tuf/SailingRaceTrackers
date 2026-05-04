fe01df0a72c065eb42ba28a9c94bab89f1e44350
Stéphane Tuffier <stephane.tuffier@sund.ku.dk>
2026-05-04 12:25:31 +0200

    Add TWS, TWD, Dist 4h, Dist 24h to sailing stats table

--- app.py
+++ app.py
@@ -157,17 +157,13 @@ with tab1:
             "boatClass": "Boat Class",
             "dtf": "DTF",
             "dtl": "DTL",
-            "tws": "TWS",
-            "twd": "TWD",
-            "dist4h": "Dist 4h",
-            "dist24h": "Dist 24h",
         }
     )
 
     # Show table
     st.dataframe(
         display_df[
-            ["Rank", "boatName", "Class", "Class Rank", "Boat Class", "DTF", "DTL", "TWS", "TWD", "Dist 4h", "Dist 24h"]
+            ["Rank", "boatName", "Class", "Class Rank", "Boat Class", "DTF", "DTL"]
         ],
         width="stretch",
         height=600,
@@ -184,7 +180,14 @@ with tab1:
         boat_id = str(boat_row["boat"])
         hour_stats = precomputed_stats.get(boat_id, {})
 
-        stats_row = {"boatName": boat_row["boatName"], "Class": boat_row["classType"]}
+        stats_row = {
+            "boatName": boat_row["boatName"],
+            "Class": boat_row["classType"],
+            "TWS": boat_row.get("tws"),
+            "TWD": boat_row.get("twd"),
+            "Dist 4h": boat_row.get("dist4h"),
+            "Dist 24h": boat_row.get("dist24h"),
+        }
 
         for hours in [1, 4, 24]:
             hs = hour_stats.get(hours, {})
@@ -197,12 +200,14 @@ with tab1:
 
     speed_cols = ["1h Speed", "4h Speed", "24h Speed"]
     vmg_cols = ["1h VMG", "4h VMG", "24h VMG"]
-    all_numeric_cols = speed_cols + vmg_cols
+    all_numeric_cols = speed_cols + vmg_cols + ["Dist 4h", "Dist 24h"]
 
     format_dict = {col: "{:.1f}" for col in all_numeric_cols}
+    format_dict["TWS"] = "{:.0f}"
+    format_dict["TWD"] = "{:.0f}"
 
     st.dataframe(
-        stats_df.style.format(format_dict, na_rep="-").background_gradient(
+        stats_df[["boatName", "Class", "TWS", "TWD", "Dist 4h", "Dist 24h", "1h Speed", "1h VMG", "4h Speed", "4h VMG", "24h Speed", "24h VMG"]].style.format(format_dict, na_rep="-").background_gradient(
             cmap="RdYlGn", subset=all_numeric_cols, vmin=0
         ),
         width="stretch",
